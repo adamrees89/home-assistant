34cfd9d30948b6c799a413e3e9f141a842d50c79
Adam Rees <adamrees89@users.noreply.github.com>
2017-01-05 16:42:09 +0000

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "lat: 51" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "lon: -3" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "WUndergroundAPI: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ILOUser: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ILOPassword: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
