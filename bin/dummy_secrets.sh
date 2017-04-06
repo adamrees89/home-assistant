134b2c93873276ec7bc30d251baf5b181c07547c
Adam Rees <adamrees89@users.noreply.github.com>
2017-04-06 11:45:09 +0100

    Update dummy_secrets.sh
    Updated email addresses for HaveIBeenPwned sensor, and IFTTT key

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -8,3 +8,5 @@ echo "BChromecastIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "BTVIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "LTVIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ifttt_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "email1: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "email2: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
