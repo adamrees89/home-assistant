34b309ccdf74c585ea001b78dc18adce13f0191c
Adam Rees <adamrees89@users.noreply.github.com>
2017-04-06 11:42:50 +0100

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -7,3 +7,4 @@ echo "ILOIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "BChromecastIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "BTVIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "LTVIP: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "ifttt_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
